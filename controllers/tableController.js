96d5a7c01e9abed8baf04682f7b1f881b1e61e8c
sabaath <babic.5832@gmail.com>
2023-04-22 12:51:25 +0200

    hopefully working main

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -1,13 +1,31 @@
-import { createTables } from "/views/index/script.js";
-import express from "express";
+import express from 'express';
+import {query} from "../service/MariaClient.js";
+import {authorize} from '../service/Security.js'
+import * as Table from "../service/Tables.js";
+
 
 const router = express.Router();
-router.post("/create/:name", async function (req, res) {
-    console.log(req.body.tableSchema, req.params.name);
-    await createTables(req.params.name, req.body.tableSchema);
-    await req.flash("success", "Tabuľka bola vytvorená.");
 
-    // Redirect to an appropriate page after creating the table
-    res.redirect("/create");
+
+router.post("/droptab",  async function (req, res) {
+    // pockat na dokoncenie funkcie pre pridanie prispevku
+    console.log("tusom");
+    await Table.dropTables(req.body.remove);
+    await req.flash('success', 'Tabuľka bola vymazaná.')
+
+    // presmerovat na zobrazenie vsetkych prispevkov
+    res.redirect('/vytvor');
 });
- export { router as tableController }
+
+router.post("/update",  async function (req, res) {
+    // pockat na dokoncenie funkcie pre pridanie prispevku
+    console.log(req.body.update);
+    await Table.updateTables(req.body.update);
+    await req.flash('success', 'Tabuľka bola upravená.')
+
+    // presmerovat na zobrazenie vsetkych prispevkov
+    res.redirect('/vytvor');
+});
+
+
+export {router as TableController}
